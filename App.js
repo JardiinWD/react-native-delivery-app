d08b7afa64dd8c871e43e5661e32d20ea8e9b986
Alessandro Pecorilla <alessandro.pecorilla-ext@temera.it>
2023-09-09 16:45:08 +0200

    Created CustomDrawer component

--- App.js
+++ App.js
@@ -2,6 +2,33 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 
+
+import CustomDrawer from './navigation/CustomDrawer'
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Home'}
+            >
+                <Stack.Screen
+                    name="Home"
+                    component={CustomDrawer}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+
+
+
+/* 
 import { MainLayout } from './screens'
 
 const Stack = createStackNavigator();
@@ -22,6 +49,6 @@ const App = () => {
             </Stack.Navigator>
         </NavigationContainer>
     )
-}
+} */
 
 export default App
--- constants/dummyData.js
+++ constants/dummyData.js
@@ -1,7 +1,7 @@
 import { icons, images } from "./";
 
 const myProfile = {
-    name: "ByProgrammers",
+    name: "Alessandro Pecorilla",
     profile_image: images.profile,
     address: "No. 88, Jln Padungan, Kuching"
 }
